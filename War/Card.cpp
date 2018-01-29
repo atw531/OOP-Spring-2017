0090c5584e0b2cc911a45bae31a3621d1ed1c2ec
Alex <walenchoka@gmail.com>
2018-01-29 09:59:51 -0500

    Created a shell for a card class with an enumerator for Rank and Suit.

---
+++ War/Card.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alex on 1/29/2018.
+//
+
+#include "Card.hpp"
---
+++ War/Card.hpp
@@ -0,0 +1,37 @@
+#ifndef WAR_CARD_HPP
+#define WAR_CARD_HPP
+
+
+enum Suit {
+    Diamonds,
+    Hearts,
+    Spades,
+    Clubs
+};
+
+enum Rank {
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King
+};
+
+class Card {
+
+public:
+
+private:
+
+};
+
+
+#endif //WAR_CARD_HPP
