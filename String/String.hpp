4991e9fd5c38e7735045ceb832b3c8ae255aa43c
Alex <walenchoka@gmail.com>
2018-02-07 19:33:49 -0500

    Created String class header and implementation files with default and copy constructors.

---
+++ String/String.hpp
@@ -0,0 +1,29 @@
+#include <cstring>
+
+#ifndef STRING_STRING_HPP
+#define STRING_STRING_HPP
+
+
+class String {
+public:
+
+    // Constructor
+    String(const char* c)
+        : strLength(std::strlen(c)), mString(new char[strLength]) {
+        std::strcpy(mString, c);
+    }
+
+    // Copy Constructor
+    String(const String& s)
+        : mString(s.mString), strLength(std::strlen(mString)) {
+
+    }
+
+private:
+    char* mString;
+    int strLength;
+
+};
+
+
+#endif
---
+++ String/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "String.hpp"
+
+int main() {
+    String s1 = "test";
+    String s2 = s1;
+    
+    return 0;
+}
